b99b2212458f727cad85b80045b58278cd842ef7
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-05-06 16:55:18 -0300

    Exerc. 7 a 9

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 class Users extends Component {
   render() {
+    const { greetingMessage = 'Hi There',
+      match: { params: { id }
+    } } = this.props;
+      
     return (
-      <div>
-        <h2>Users</h2>
-        <p> My awesome Users component </p>
-        <Link to="/">Voltar à home</Link>
-      </div>
+      <main>
+        <h2> Users </h2>
+        <p> 
+          { `${greetingMessage} User ${id}` }, this is my awesome Users component
+        </p>
+      </main>
     );
   }
 };
